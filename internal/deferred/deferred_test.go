1651ecd702debd62a34e62555f0d92507f1d89ec
kerelape <kerelape@gmail.com>
2023-09-13 02:39:21 +0500

    Add test

--- internal/deferred/deferred_test.go
+++ internal/deferred/deferred_test.go
@@ -22,4 +22,21 @@ func TestDeferred(t *testing.T) {
 		var _, err = d.Get(ctx)
 		assert.NotNil(t, err, "expected to get an error due to context cancellation")
 	})
+	t.Run("Panics when setting a value twice", func(t *testing.T) {
+		var d Deferred[any]
+		assert.NotPanics(
+			t,
+			func() {
+				d.Set("")
+			},
+			"expected to NOT panic when setting the value for first time",
+		)
+		assert.Panics(
+			t,
+			func() {
+				d.Set("")
+			},
+			"expected to panic when setting a subsequent value",
+		)
+	})
 }
